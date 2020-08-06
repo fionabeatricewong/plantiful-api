2034ba134105820df53d64b384bcb26ad1b6f447
fionabeatricewong <fionabeatricewong@gmail.com>
2020-08-06 14:10:03 -0400

    Updated plant model and plant routes

--- app/models/plant.js
+++ app/models/plant.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
 
-// const User = require('./user')
-
 const plantSchema = new mongoose.Schema({
   name: {
     type: String,
--- app/routes/plant_routes.js
+++ app/routes/plant_routes.js
@@ -1,50 +1,80 @@
 const express = require('express')
+const passport = require('passport')
+
+// Require model:
+const Plant = require('../models/plant')
+
+const customErrors = require('../../lib/custom_errors')
+
+const handle404 = customErrors.handle404
+
+const requireOwnership = customErrors.requireOwnership
+
+const removeBlanks = require('../../lib/remove_blank_fields')
+
+const requireToken = passport.authenticate('bearer', { session: false })
+
 // instantiate a router (mini app that only handles routes)
 const router = express.Router()
 
-// Require model:
-const Plant = require('./../models/plant')
-
 // INDEX:
-router.get('/plants', (req, res) => {
+router.get('/plants', requireToken, (req, res, next) => {
   Plant.find()
-    .then(plants => res.json({ plants: plants }))
-    .catch(console.error)
+    .then(plants => {
+      return plants.map(plant => plant.toObject())
+    })
+    .then(plants => res.status(200).json({ plants: plants }))
+    .catch(next)
 })
 
 // SHOW:
-router.get('/plants/:id', (req, res) => {
+router.get('/plants/:id', requireToken, (req, res, next) => {
   const id = req.params.id
   Plant.findById(id)
-    .then(plant => res.json({ plant: plant }))
+    .then(handle404)
+    .then(plant => res.status(200).json({ plant: plant.toObject() }))
+    .catch(next)
 })
 
 // CREATE:
-router.post('/plants', (req, res) => {
+router.post('/plants', requireToken, (req, res, next) => {
+  req.body.plant.owner = req.user.id
+
   // Accept data from request:
   const plant = req.body.plant
   Plant.create(plant)
-    .then(plant => res.status(201).json({ plant: plant }))
-    .catch(console.error)
-})
-
-// DESTROY/DELETE:
-router.delete('/plants/:id', (req, res) => {
-  const id = req.params.id
-  Plant.findById(id)
-    .then(plant => plant.remove())
-    .then(res.sendStatus(204))
-    .catch(console.error)
+    .then(plant => {
+      res.status(201).json({ plant: plant.toObject() })
+    })
+    .catch(next)
 })
 
 // UPDATE:
-router.patch('/plants/:id', (req, res) => {
+router.patch('/plants/:id', requireToken, removeBlanks, (req, res, next) => {
+  delete req.body.plant.owner
   const id = req.params.id
   const data = req.body.plant
   Plant.findById(id)
-    .then(plant => plant.updateOne(data))
-    .then(res.sendStatus(200))
-    .catch(console.error)
+    .then(handle404)
+    .then(plant => {
+      requireOwnership(req, plant)
+      return plant.updateOne(data)
+    })
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
+
+// DESTROY/DELETE:
+router.delete('/plants/:id', requireToken, (req, res, next) => {
+  const id = req.params.id
+  Plant.findById(id)
+    .then(handle404)
+    .then(plant => {
+      requireOwnership(req, plant)
+      plant.deleteOne()
+    })
+    .then(() => res.sendStatus(204))
+    .catch(next)
 })
 
 module.exports = router
